fc40c6e2a6ee378cf872a0e92ae6cf649217bfac
Z1XCY <70898164+Z1XCY@users.noreply.github.com>
2021-02-11 10:16:03 +0100

    main.py int updated
    int staat nu op regel 21-22

--- main.py
+++ main.py
@@ -17,10 +17,10 @@ while running:
     #Get inputs
     os.system('cls')
     print("Rollercoaster-check™")
-    age = input("Voer leeftijd in: ")
-    height = input("Voer lengte in: ")
-    age = int(age)
-    height = int(height)
+    age = int(input("Voer leeftijd in: "))
+    height = int(input("Voer lengte in: "))
+    age = (age)
+    height = (height)
 
     #Process checks
     if(age > age_check and height > height_check):
